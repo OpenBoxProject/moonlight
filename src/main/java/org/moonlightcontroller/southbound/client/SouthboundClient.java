f6de31c6d8f1111889f827bc99bfa69921c7e793
Dan Shmidt <dan.shmidt@gmail.com>
2015-12-04 16:15:20 +0200

    Fixing REST client/server incompatibility

--- src/main/java/org/moonlightcontroller/southbound/client/SouthboundClient.java
+++ src/main/java/org/moonlightcontroller/southbound/client/SouthboundClient.java
@@ -2,20 +2,23 @@ package org.moonlightcontroller.southbound.client;
 
 import java.util.List;
 
+/*
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+*/
 import org.openboxprotocol.protocol.IStatement;
 import org.openboxprotocol.protocol.topology.InstanceLocationSpecifier;
 
 public class SouthboundClient implements ISouthboundClient {
 
-	Client client = Client.create();
+//	Client client = Client.create();
 	String getUrl = "http://localhost:8080/JAXRS-JSON/rest/student/data/get";
 	String postUrl = "http://localhost:8080/JAXRS-JSON/rest/student/data/post";
 
 	// an example will be removed
 	public void getRequest(){
+		/*
 		WebResource webResource = client.resource(getUrl);
 		ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
 		if(response.getStatus()!=200){
@@ -25,10 +28,12 @@ public class SouthboundClient implements ISouthboundClient {
 		String result = response.getEntity(String.class);
 		System.out.println("Response from the Server: ");
 		System.out.println(result);
+		*/
 	}
 	
 	// an example will be removed
 	public void postRequest(){
+		/*
 		WebResource webResource = client.resource(postUrl);
 		String inputData = "{\"firstName\":\"Alice\",\"lastName\":\"Brown\",\"school\":\"Bright Stars\",\"standard\":\"Three\",\"rollNumber\":1212}";
 		ClientResponse response = webResource.type("application/json").post(ClientResponse.class,inputData);
@@ -39,6 +44,7 @@ public class SouthboundClient implements ISouthboundClient {
 		String result = response.getEntity(String.class);
 		System.out.println("Response from the Server: ");
 		System.out.println(result);
+		*/
 	}
 
 	@Override
