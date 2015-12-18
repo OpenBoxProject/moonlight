386b9566341fc331c7a63c69df714f2321749bc4
Dan Shmidt <dan.shmidt@gmail.com>
2015-12-18 11:39:01 +0200

    Aggregator Merge Phase 2.4: Removing duplicate files from temp

--- src/main/java/org/moonlightcontroller/aggregator/temp/BlockClass.java
+++
@@ -1,9 +0,0 @@
-package org.moonlightcontroller.aggregator.temp;
-
-public enum BlockClass {
-	BLOCK_CLASS_STATIC,
-	BLOCK_CLASS_CLASSIFIER,
-	BLOCK_CLASS_MODIFIER,
-	BLOCK_CLASS_SHAPER,
-	BLOCK_CLASS_TERMINAL
-}
