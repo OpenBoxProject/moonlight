206c20b7af83764c98f26724a99c3275ee69e6bb
roy.fr <roy.fr@matomy.com>
2018-11-02 18:40:44 +0200

    dashboard deployment api

--- src/main/java/org/moonlightcontroller/exceptions/ApplicationsLoadException.java
+++ src/main/java/org/moonlightcontroller/exceptions/ApplicationsLoadException.java
@@ -4,4 +4,8 @@ public class ApplicationsLoadException extends Exception {
     public ApplicationsLoadException() {
         super("Failed to update Applications");
     }
+
+    public ApplicationsLoadException(String message) {
+        super(message);
+    }
 }
--- src/main/java/org/openbox/dashboard/DashboardServerApi.java
+++ src/main/java/org/openbox/dashboard/DashboardServerApi.java
@@ -15,11 +15,13 @@ import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Path("/")
 public class DashboardServerApi {
@@ -105,25 +107,69 @@ public class DashboardServerApi {
 		return resp;
 	}
 
+	private final static String APPS_REPOSITORY_DIRECTORY = "./apps-repo";
+	private final static String DEPLOYED_APPS_DIRECTORY = "./apps";
+
 	@GET
+    @Path("listRepositoryApps")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> listRepositoryApps() throws ApplicationsLoadException {
+        return FileUtils.listFiles(new File(APPS_REPOSITORY_DIRECTORY), new String[]{"jar"}, false).stream().map(File::getName).sorted().collect(Collectors.toList());
+    }
+
+    @POST
     @Path("reloadApplications")
     public void reloadApplications() throws ApplicationsLoadException {
         ApplicationsManager.getInstance().updateApps();
     }
 
     @POST
-    @Path("/uploadApplication")
+    @Path("deployApplications")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deployApplications(List<String> apps) throws ApplicationsLoadException, IOException {
+        File tempDir = null;
+        try {
+            tempDir = Files.createTempDirectory("apps").toFile();
+
+            for (String app : apps) {
+                File srcFile = new File(APPS_REPOSITORY_DIRECTORY + "/" + app);
+                File dstFile = new File(tempDir + "/" + app);
+                if (!srcFile.exists())
+                    return Response.status(409).entity("Application not found in repository directory: " + app).build();
+
+                FileUtils.copyFile(srcFile, dstFile);
+            }
+
+            FileUtils.listFiles(new File(DEPLOYED_APPS_DIRECTORY), new String[]{"jar"}, false).forEach(File::delete);
+            FileUtils.copyDirectory(tempDir, new File(DEPLOYED_APPS_DIRECTORY));
+
+            reloadApplications();
+
+            return Response.status(200).build();
+
+        } finally {
+            try {
+                FileUtils.deleteDirectory(tempDir);
+            }
+            catch (Exception ignore) {}
+        }
+
+
+
+    }
+
+    @POST
+    @Path("uploadApplication")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response uploadFile(
             @FormDataParam("file") InputStream uploadedInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException, ApplicationsLoadException {
 
-        String uploadedFileLocation = "./apps/" + fileDetail.getFileName();
+        String uploadedFileLocation = "./apps-repo/" + fileDetail.getFileName();
 
         // save it
         FileUtils.copyInputStreamToFile(uploadedInputStream, new File(uploadedFileLocation));
 
-        reloadApplications();
         return Response.status(200).build();
 
     }
